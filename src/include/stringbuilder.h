b7072ba0928723cb80400f8fd5164befcad1112e
Mariusz Lapinski <isameru@gmail.com>
2017-05-06 16:22:15 +0200

    Let clang use experimental/string_view

--- src/include/stringbuilder.h
+++ src/include/stringbuilder.h
@@ -30,7 +30,7 @@
 #include <numeric>
 #include <utility>
 #include <type_traits>
-#ifdef __GNUC__
+#if defined(__GNUC__) || defined(__clang__)
 #include <experimental/string_view>
 namespace std {
     template<typename... AnyT> using basic_string_view = experimental::basic_string_view<AnyT...>;
