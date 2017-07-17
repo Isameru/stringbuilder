8387bf9a73083981c2c3c2e4b2cf69dff504da29
Isameru <isameru@gmail.com>
2017-07-17 21:17:48 +0200

    Fix gcc-6 include error

--- src/include/stringbuilder.h
+++ src/include/stringbuilder.h
@@ -30,7 +30,7 @@
 #include <numeric>
 #include <utility>
 #include <type_traits>
-#if defined(__GNUC__) && !defined(__clang__) && __cplusplus <= 201402L
+#if defined(__GNUC__) && !defined(__clang__) && __cplusplus < 201703L
 #include <experimental/string_view>
 namespace std {
     template<typename... AnyT> using basic_string_view = experimental::basic_string_view<AnyT...>;
